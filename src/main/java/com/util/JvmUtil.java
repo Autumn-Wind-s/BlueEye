d4f9483ac23f431f44e65631feb97c2556f80d34
songjinjin <6020202300@qq.com>
2023-02-25 15:06:20 +0800

    新增jvm工具类

---
+++ src/main/java/com/util/JvmUtil.java
@@ -0,0 +1,35 @@
+package com.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @Author SDJin
+ * @CreationDate 2023/2/24 18:07
+ * @Description ：
+ */
+public class JvmUtil {
+    static int vmid;
+
+    static {
+        //获取jvm的进程id
+        try {
+            Process jps = Runtime.getRuntime().exec("jps");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(jps.getInputStream()));
+            String s = "";
+            while ((s = bufferedReader.readLine()) != null) {
+                if (s.indexOf("Launcher") != -1) {
+                    break;
+                }
+            }
+            vmid = Integer.parseInt(s.substring(0, s.indexOf(" ")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int getVmid() {
+        return vmid;
+    }
+}
