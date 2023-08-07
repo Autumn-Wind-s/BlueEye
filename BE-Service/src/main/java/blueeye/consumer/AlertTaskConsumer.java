c26c289424ce22f396507e3288d8f32d0bb6b64f
songjinjin <6020202300@qq.com>
2023-08-07 15:51:17 +0800

    新增报警系统的报警任务消费类

---
+++ BE-Service/src/main/java/blueeye/consumer/AlertTaskConsumer.java
@@ -0,0 +1,57 @@
+package blueeye.consumer;
+
+import blueeye.context.BlueEyeContext;
+import blueeye.email.MailSender;
+import blueeye.note.NoteSender;
+import blueeye.pojo.alert.AlertMethod;
+import blueeye.pojo.alert.AlertRecord;
+import blueeye.pojo.task.impl.alert.AlertTask;
+import blueeye.service.AlertRecordService;
+import blueeye.wx.WxMsSender;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @Author SDJin
+ * @CreationDate 2023/6/27 15:20
+ * @Description ：
+ */
+public class AlertTaskConsumer extends Thread {
+    @Override
+    public void run() {
+        while (true) {
+            AlertTask task =null;
+            try {
+                task = AlertRecordService.queue.take();
+            } catch (InterruptedException e) {
+                //线程阻塞被打断，继续开始
+                continue;
+            }
+            //从数据中心获取自增ID
+            AlertRecord alertRecord = new AlertRecord(1, task.getTaskId());
+            alertRecord.setAlertMethod(task.getMethod());
+            alertRecord.setNotifier(task.getNotifier());
+            AlertMethod method = task.getMethod();
+            try {
+                if (method.equals(AlertMethod.WX)){
+                    alertRecord.setAlertTime(new Timestamp(System.currentTimeMillis()));
+                    alertRecord.setContent("报警内容："+task.getContent()+"\n操作结果："+ WxMsSender.doSendMessage(task));
+                } else if (method.equals(AlertMethod.NOTE)) {
+                    alertRecord.setAlertTime(new Timestamp(System.currentTimeMillis()));
+                    alertRecord.setContent("报警内容："+task.getContent()+"\n操作结果："+ NoteSender.doSendMessage(task));
+                }else if(method.equals(AlertMethod.EMail)){
+                    alertRecord.setAlertTime(new Timestamp(System.currentTimeMillis()));
+                    alertRecord.setContent("报警内容："+task.getContent()+"\n操作结果："+ MailSender.doSendMessage(task));
+                }else {
+                    // todo 电话报警
+                }
+            } catch (InterruptedException | IOException | ExecutionException e) {
+                alertRecord.setContent("报警内容："+task.getContent()+"\n操作结果："+ e.getMessage());
+            }
+            //  将AlertRecord添加到数据中心
+            BlueEyeContext.dataCenter.getRecords().addAlertRecord(alertRecord);
+        }
+    }
+}
