e9fe0d7e605a0592fd43d5b1a11bded80ac1d2f1
songjinjin <6020202300@qq.com>
2023-08-07 15:41:26 +0800

    新增user类

--- BE-Common/src/main/java/blueeye/pojo/user/User.java
+++ BE-Common/src/main/java/blueeye/pojo/user/User.java
@@ -1,12 +1,16 @@
-package com.pojo;
+package blueeye.pojo.user;
+
+import lombok.Data;
 
 /**
  * @Author SDJin
  * @CreationDate 2023/2/8 16:36
  * @Description ：
  */
+@Data
 public class User {
     private String name;
     private String password;
     private Integer role;
+
 }
