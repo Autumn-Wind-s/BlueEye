5d84dbd44036788589cec74acf269c04ee0b8da6
songjinjin <6020202300@qq.com>
2023-08-07 15:41:48 +0800

    新增Common模块测试类

---
+++ BE-Common/src/test/java/Test.java
@@ -0,0 +1,18 @@
+import blueeye.pojo.task.Task;
+import blueeye.pojo.task.impl.monitor.MonitorTask;
+import blueeye.pojo.task.impl.script.ScriptTask;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+/**
+ * @Author SDJin
+ * @CreationDate 2023/7/22 21:26
+ * @Description ：
+ */
+public class Test {
+    public static void main(String[] args) {
+        String a="false";
+        System.out.println(Boolean.parseBoolean(a));
+    }
+}
