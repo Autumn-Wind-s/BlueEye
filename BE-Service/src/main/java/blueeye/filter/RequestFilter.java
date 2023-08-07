29144804255c0af15c69466400ccb41bed1b73ea
songjinjin <6020202300@qq.com>
2023-08-07 15:50:08 +0800

    新增Request过滤类

--- BE-Service/src/main/java/blueeye/filter/RequestFilter.java
+++ BE-Service/src/main/java/blueeye/filter/RequestFilter.java
@@ -1,22 +1,26 @@
-package com.monitor.request; /**
+package blueeye.filter;
+/**
  * @Author SDJin
  * @CreationDate 2022/11/3 20:38
  * @Description ：
  */
 
+import blueeye.context.BlueEyeContext;
 import com.wujiuye.flow.common.TimeUtil;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.*;
-import javax.servlet.annotation.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//@WebFilter(filterName = "RequestFilter")
+@WebFilter(filterName = "RequestFilter")
+@Slf4j
 public class RequestFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.println("qps过滤器初始化");
+        log.info("qps过滤器初始化");
     }
 
     @Override
@@ -25,15 +29,15 @@ public class RequestFilter implements Filter {
         HttpServletResponse res = (HttpServletResponse) response;
         String requestURI = req.getRequestURI();
         requestURI = requestURI.replaceFirst(req.getContextPath(), "");
-        System.out.println("接口被访问" + requestURI);
+        log.info("接口被访问" + requestURI);
         long startTime = TimeUtil.currentTimeMillis();
         chain.doFilter(request, response);
         long rt = TimeUtil.currentTimeMillis() - startTime;
         int status = ((HttpServletResponse) response).getStatus();
         if (status > 400) {
-            RequestMonitor.getInstance().take(requestURI).incrException();
+            BlueEyeContext.dataCenter.uploadInterfaceData(requestURI,rt,false);
         } else {
-            RequestMonitor.getInstance().take(requestURI).incrSuccess(rt);
+            BlueEyeContext.dataCenter.uploadInterfaceData(requestURI,rt,true);
         }
     }
 
